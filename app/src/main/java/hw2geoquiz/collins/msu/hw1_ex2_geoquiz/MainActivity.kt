e134767f59cb4a9692d07b5782965b9b3dfceeee
James Collins <jamescollinscpt@gmail.com>
2023-02-18 19:04:21 -0600

    GeoQuiz, Homework 2, Exercise 3, first commit

---
+++ app/src/main/java/hw2geoquiz/collins/msu/hw1_ex2_geoquiz/MainActivity.kt
@@ -0,0 +1,78 @@
+package hw2geoquiz.collins.msu.hw1_ex2_geoquiz
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.collins.msu.hw1_ex2_geoquiz.R
+import com.collins.msu.hw1_ex2_geoquiz.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import hw2geoquiz.Question
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    //private lateinit var trueButton : Button
+   // private lateinit var falseButton : Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //trueButton = findViewById(R.id.true_button)
+        //falseButton = findViewById(R.id.false_button)
+
+
+        binding.trueButton.setOnClickListener {
+            val snackBar = Snackbar.make(
+                it,
+                "Correct",
+                Snackbar.LENGTH_LONG
+            )
+            snackBar.show()
+        }
+
+        binding.falseButton.setOnClickListener {
+            val snackBar = Snackbar.make(
+                it,
+                "Incorrect",
+                Snackbar.LENGTH_LONG
+            )
+            snackBar.setTextColor(Color.BLACK)
+            snackBar.setBackgroundTint(Color.RED)
+            snackBar.show()
+        }
+
+        binding.previousButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            //val questionTextResId = questionBank[currentIndex].textResId
+            //binding.questionTextview.setText(questionTextResId)
+            updateQuestion()
+        }
+
+        binding.nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            //val questionTextResId = questionBank[currentIndex].textResId
+            //binding.questionTextview.setText(questionTextResId)
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextview.setText(questionTextResId)
+    }
+}
